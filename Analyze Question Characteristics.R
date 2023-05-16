6e93a58411786a238baeec88d48c2486cc79e994
Oliver Tang <38697621+oliverytang@users.noreply.github.com>
2023-05-16 01:36:46 -0400

    Add files via upload

--- Analyze Question Characteristics.R
+++ Analyze Question Characteristics.R
@@ -1,13 +1,43 @@
 rm(list=ls())
 
-# Input spreadsheet should be .csv file with 3 columns: Correct (binary: 0 for incorrect answer vs. 1 for correct answer), High_Order (binary: 0 for first-order question vs. 1 for higher-order question), Words (numeric: total number of words in question)
+library(irr)
+
+# Input spreadsheet (named core) should be .csv file with 6 columns: Correct_GPT (binary: 0 for incorrect answer vs. 1 for correct answer by ChatGPT), Correct_4 (binary: 0 for incorrect answer vs. 1 for correct answer by GPT-4), Correct_4_Replicate (binary: 0 for incorrect answer vs. 1 for correct answer by GPT-4), High_Order (binary: 0 for first-order question vs. 1 for higher-order question), Words (numeric: total number of words in question), Imaging (binary: 0 for no imaging in question stem vs. 1 for imaging in question stem)
 # Each row should correspond with a test question
 
+# Compare performance between human users, ChatGPT, and GPT-4
+length = 500 # User-defined total length of dataset
+user = 368.6431 # User-defined average number of questions answered correctly by user
+prop.test(x=c(user, sum(core$Correct_GPT)), n=c(length, length))
+prop.test(x=c(user, sum(core$correct_4)), n=c(length, length))
+prop.test(x=c(sum(core$Correct_GPT), sum(core$correct_4)), n=c(length, length))
+
 # Compare word count between correct and incorrect answers
-t.test(Words~Correct, data=core)
+t.test(Words~Correct_GPT, data=core)
+t.test(Words~Correct_4, data=core)
 
-# Analyze association between question word count and ChatGPT choosing the correct answer
-summary(glm(Correct~Words, data=core, family="binomial"))
+# Analyze association between question word count and LLM choosing the correct answer
+summary(glm(Correct_GPT~Words, data=core, family="binomial"))
+summary(glm(Correct_4~Words, data=core, family="binomial"))
 
-# Analyze association between higher-order question status and ChatGPT choosing the correct answer
-summary(glm(Correct~High_Order, data=core, family="binomial"))
+# Analyze association between higher-order question status and LLM choosing the correct answer
+summary(glm(Correct_GPT~High_Order, data=core, family="binomial"))
+summary(glm(Correct_4~High_Order, data=core, family="binomial"))
+
+# Analyze differences in performance between ChatGPT and GPT-4 by question higher-order status
+matrix = matrix(c(table(core$Correct_GPT[which(core$Order==0)]), table(core$Correct_4[which(core$Order==0)])), ncol=2, nrow=2)
+chisq.test(matrix)
+matrix = matrix(c(table(core$Correct_GPT[which(core$Order==1)]), table(core$Correct_4[which(core$Order==1)])), ncol=2, nrow=2)
+chisq.test(matrix)
+
+# Analyze association between question word count and LLM choosing the correct answer
+summary(glm(Correct_GPT~Imaging, data=core, family="binomial"))
+summary(glm(Correct_4~Imaging, data=core, family="binomial"))
+
+# Analyze differences in performance between ChatGPT and GPT-4 on imaging questions
+matrix = matrix(c(table(core$Correct_GPT[which(core$Imaging==1)]), table(core$Correct_4[which(core$Imaging==1)])), ncol=2, nrow=2)
+chisq.test(matrix)
+
+# Analyze differences in performance and concordance between two separate trials of GPT-4
+prop.test(c(sum(core$Correct_4), sum(core$Correct_4_Replicate)), c(length, length))
+kappa2(core[, c("Correct_4", "Correct_4_Replicate")])
--- Analyze Subspecialty Scores.R
+++ Analyze Subspecialty Scores.R
@@ -1,20 +1,75 @@
 rm(list=ls())
 
-# Input spreadsheet should be .csv file with 3 columns: Category (string: name of question subspecialty), Correct (numeric: total number of correct questions in subspecialty), Total (numeric: total number of questions in subspecialty)
+# Input spreadsheet should be .csv file with 5 columns: Category (string: name of question subspecialty), Correct_GPT (numeric: total number of correct answers by ChatGPT in subspecialty), Correct_4 (numeric: total number of correct answers by GPT-4 in subspecialty), Correct_Human_Percent (numeric: percentage correct for each score, ranging from 0 to 100), Total (numeric: total number of questions in subspecialty)
 # First row should be overall score on exam
 
 core = read.csv("Analyze Subspecialty Scores.csv") # Change file path to your spreadsheet
 
-# Calculate percentage correct for each score
-core$Percentage = core$Correct / core$Total * 100
+# Generate number of questions answered correctly by human users
+core$Correct_Human = core$Total * core$Correct_Human_Percent
 
-# Calculate 95% confidence interval for overall score
-prop.test(core[1, "Correct"], core[1, "Total"])
+# Calculate percentage correctand 95% confidence interval for each score
+core$Percentage = core$Correct_Human / core$Total * 100
+prop.test(core[1, "Correct_Human"], core[1, "Total"])
+core$Percentage = core$Correct_GPT / core$Total * 100
+prop.test(core[1, "Correct_GPT"], core[1, "Total"])
+core$Percentage = core$Correct_4 / core$Total * 100
+prop.test(core[1, "Correct_4"], core[1, "Total"])
 
-# Calculate whether performance on subspecialty is significantly different from performance on rest of exam
+# Calculate differences between humans, ChatGPT, and GPT-4 for each subspecialty
 for (row in 2:nrow(core)) {
   print(core$Category[row])
-  data = c(core$Correct[row], core$Total[row] - core$Correct[row], sum(core$Correct[-row]), sum(core$Total[-row]) - sum(core$Correct[-row]))
+  data = c(core$Correct_Human[row], core$Total[row] - core$Correct_Human[row], core$Correct_GPT[row], core$Total[row] - core$Correct_GPT[row])
+  test = matrix(data, 2, 2)
+  if (core[row, "Total"] < 20) {
+    print(fisher.test(test))
+  } else {
+    print(chisq.test(test))
+  }
+  data = c(core$Correct_Human[row], core$Total[row] - core$Correct_Human[row], core$Correct_4[row], core$Total[row] - core$Correct_4[row])
+  test = matrix(data, 2, 2)
+  if (core[row, "Total"] < 20) {
+    print(fisher.test(test))
+  } else {
+    print(chisq.test(test))
+  }
+  data = c(core$Correct_GPT[row], core$Total[row] - core$Correct_GPT[row], core$Correct_4[row], core$Total[row] - core$Correct_4[row])
+  test = matrix(data, 2, 2)
+  if (core[row, "Total"] < 20) {
+    print(fisher.test(test))
+  } else {
+    print(chisq.test(test))
+  }
+}
+
+# Calculate whether performance on subspecialty for humans is significantly different from performance on rest of exam
+for (row in 2:nrow(core)) {
+  print(core$Category[row])
+  data = c(core$Correct_Human[row], core$Total[row] - core$Correct_Human[row], sum(core$Correct_Human[-row]), sum(core$Total[-row]) - sum(core$Correct_Human[-row]))
+  test = matrix(data, 2, 2)
+  if (core[row, "Total"] < 20) {
+    print(fisher.test(test))
+  } else {
+    print(chisq.test(test))
+  }
+}
+
+# Calculate whether performance on subspecialty for ChatGPT is significantly different from performance on rest of exam
+for (row in 2:nrow(core)) {
+  print(core$Category[row])
+  data = c(core$Correct_GPT[row], core$Total[row] - core$Correct_GPT[row], sum(core$Correct_GPT[-row]), sum(core$Total[-row]) - sum(core$Correct_GPT[-row]))
+  test = matrix(data, 2, 2)
+  if (core[row, "Total"] < 20) {
+    print(fisher.test(test))
+  } else {
+    print(chisq.test(test))
+  }
+}
+
+# Calculate whether performance on subspecialty for GPT-4 is significantly different from performance on rest of exam
+for (row in 2:nrow(core)) {
+  print(core$Category[row])
+  data = c(core$Correct_4[row], core$Total[row] - core$Correct_4[row], sum(core$Correct_4[-row]), sum(core$Total[-row]) - sum(core$Correct_4[-row]))
   test = matrix(data, 2, 2)
   if (core[row, "Total"] < 20) {
     print(fisher.test(test))
